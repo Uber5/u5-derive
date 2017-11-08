c973f839b5b9c413e813b5267b3d37d7b7a600fe
Chris Oloff <christian.oloff@gmail.com>
2017-11-08 14:49:19 +0200

    Updated doc script

--- scripts/update-docs.sh
+++ scripts/update-docs.sh
@@ -1,3 +1,22 @@
 #!/bin/bash -ex
 
+# We have this script in the gh-pages branch only, and we should run it only
+# there.
+
+CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
 VERSION=`git describe --match "v[0-9]*" --abbrev=0 HEAD`
+
+if [ $CURRENT_BRANCH != gh-pages ]; then
+  echo "Must be run on the 'gh-pages' branch"
+  exit 1
+fi
+
+git merge master
+git commit -m "merged master into doc branch"
+rm -rf public/current/* public/versioned/$VERSION
+mkdir public/versioned/$VERSION
+npm run doc
+cp -r ./docs/* ./public/current/
+cp -r ./docs/* ./public/versioned/$VERSION/
+
+echo "done"
