ba4b7bcf4f7c382f9eb0e3aef70e5bad0f670392
Chris <chris@uber5.com>
2017-12-10 08:21:28 +0200

    export 'domainMongo'

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import { update, resync } from './update'
 import { tailAndInvalidate } from './tail'
 import startRepl from './start-repl'
 import createDomain from './create-domain'
+import domainMongo from './domain-mongo'
 
 export {
-  Cache, update, resync, tailAndInvalidate, startRepl, createDomain
+  Cache, update, resync, tailAndInvalidate, startRepl, createDomain, domainMongo
 }
