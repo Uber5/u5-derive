9e9b66e675834d1fcf0b2b0b347979518c831b15
Chris <chris@uber5.com>
2018-05-26 09:13:32 +0200

    May fix sporadically failing test that did a 'full table scan'

--- src/__tests__/separate-types-spec.js
+++ src/__tests__/separate-types-spec.js
@@ -4,7 +4,7 @@ import { mongo } from './config'
 
 describe('separate types', () => {
 
-  let update, thingsCollection
+  let update, thingsCollection, someKey
   
   beforeEach(() => mongo.then(client => client.db()).then(db => {
 
@@ -17,24 +17,24 @@ describe('separate types', () => {
 
     update = key => _update(cache, domain, key)
     thingsCollection = db.collection('things')
-
+    someKey = `${ new Date().getTime() }-${Math.random()}` // dirty: unique'ish key
   }))
   
   const setupData = async () => {
     const db = await mongo
     // await thingsCollection.remove({})
-    await thingsCollection.insert({ bla: 42 })
+    await thingsCollection.insert({ someKey, bla: 42 })
   }
 
   const updateAll = async () => {
-    const things = await thingsCollection.find().toArray()
-    return Promise.all(things.map(thing => update(thing._id)))
+    const things = await thingsCollection.find({ someKey }).toArray()
+    return await Promise.all(things.map(thing => update(thing._id)))
   }
 
   it('works', async () => {
     await setupData()
-    await updateAll()
-    const things = await thingsCollection.find({}).toArray()
+    const updateAllResult = await updateAll()
+    const things = await thingsCollection.find({ someKey }).toArray()
     things.map(thing => expect(thing._D.value).toBe('some value'))
   })
 })
